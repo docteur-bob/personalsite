2dbeeb6856042899337d69f6798d8b5112a7dd0e
mehdi <mehdi.aoussat@gmail.com>
2019-04-17 09:12:26 +0200

    1.0

--- book/urls.py
+++ book/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.landingpage, name="accueil"),
     path('projet/<str:tag>/',views.page_projet, name="page_projet"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- personal_site/settings.py
+++ personal_site/settings.py
@@ -118,4 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'book/media')
 # Extra places for collectstatic to find static files.
