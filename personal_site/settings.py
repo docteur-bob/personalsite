396ddd041d4c7797f526b50d4b29debe01cdb06c
mehdi <mehdi.aoussat@gmail.com>
2019-04-18 09:39:28 +0200

    pythonanywhere

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -15,7 +15,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 
 
 
@@ -26,9 +25,9 @@ PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 SECRET_KEY = 'qy%$ik$by$xqj_(q)*y7)70u60us&1ltontflon+6)$4l3ug^u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["docteurbob.pythonanywhere.com","localhost:8000","portefoliomehdiaoussat.herokuapp.com"]
+ALLOWED_HOSTS = ["docteurbob.pythonanywhere.com","localhost"]
 
 
 # Application definition
@@ -44,7 +43,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,14 +119,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'book/static')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'book/media')
 # Extra places for collectstatic to find static files.
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
