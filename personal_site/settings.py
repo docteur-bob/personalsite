653b446b34b3ff1e04a019022b8811a6733f44d9
mehdi <mehdi.aoussat@gmail.com>
2019-06-30 09:19:15 +0200

    Heroku

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -17,10 +17,8 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# ...
 if os.environ.get('ENV') == 'PRODUCTION':
-    # ...
+
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
 
