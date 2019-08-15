c295127d5c4a39bfaccf493cc97a46a26deea290
mehdi <mehdi.aoussat@gmail.com>
2019-08-15 20:47:48 +0200

    O

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -89,10 +89,6 @@ DATABASES = {
     }
 }
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
